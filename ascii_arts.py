326de7a2fc86fcd34a78ea8f3f3c32b0d1701e56
samheartache <20imas07@gmail.com>
2025-06-13 23:34:35 +0300

    Add file for ascii_arts

---
+++ ascii_arts.py
@@ -0,0 +1,27 @@
+LOGO = '''
+LLLLLLLLLLL                                                        tttt               FFFFFFFFFFFFFFFFFFFFFFMMMMMMMM               MMMMMMMM
+L:::::::::L                                                     ttt:::t               F::::::::::::::::::::FM:::::::M             M:::::::M
+L:::::::::L                                                     t:::::t               F::::::::::::::::::::FM::::::::M           M::::::::M
+LL:::::::LL                                                     t:::::t               FF::::::FFFFFFFFF::::FM:::::::::M         M:::::::::M
+  L:::::L                 aaaaaaaaaaaaa      ssssssssss   ttttttt:::::ttttttt           F:::::F       FFFFFFM::::::::::M       M::::::::::M
+  L:::::L                 a::::::::::::a   ss::::::::::s  t:::::::::::::::::t           F:::::F             M:::::::::::M     M:::::::::::M
+  L:::::L                 aaaaaaaaa:::::ass:::::::::::::s t:::::::::::::::::t           F::::::FFFFFFFFFF   M:::::::M::::M   M::::M:::::::M
+  L:::::L                          a::::as::::::ssss:::::stttttt:::::::tttttt           F:::::::::::::::F   M::::::M M::::M M::::M M::::::M
+  L:::::L                   aaaaaaa:::::a s:::::s  ssssss       t:::::t                 F:::::::::::::::F   M::::::M  M::::M::::M  M::::::M
+  L:::::L                 aa::::::::::::a   s::::::s            t:::::t                 F::::::FFFFFFFFFF   M::::::M   M:::::::M   M::::::M
+  L:::::L                a::::aaaa::::::a      s::::::s         t:::::t                 F:::::F             M::::::M    M:::::M    M::::::M
+  L:::::L         LLLLLLa::::a    a:::::assssss   s:::::s       t:::::t    tttttt       F:::::F             M::::::M     MMMMM     M::::::M
+LL:::::::LLLLLLLLL:::::La::::a    a:::::as:::::ssss::::::s      t::::::tttt:::::t     FF:::::::FF           M::::::M               M::::::M
+L::::::::::::::::::::::La:::::aaaa::::::as::::::::::::::s       tt::::::::::::::t     F::::::::FF           M::::::M               M::::::M
+L::::::::::::::::::::::L a::::::::::aa:::as:::::::::::ss          tt:::::::::::tt     F::::::::FF           M::::::M               M::::::M
+LLLLLLLLLLLLLLLLLLLLLLLL  aaaaaaaaaa  aaaa sssssssssss              ttttttttttt       FFFFFFFFFFF           MMMMMMMM               MMMMMMMM                                      
+'''
+
+MENU = '''
+┌───────────────────────────────────────────────────┐  
+│              [1] | Parse your albums              │  
+│              [2] | Download album covers          │  
+│              [3] | Make collage                   │  
+│              [4] | Guidance on the use            │  
+└───────────────────────────────────────────────────┘   
+'''
