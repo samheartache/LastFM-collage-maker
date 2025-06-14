783af871492a7af799ec7fab2c666afda33b25f8
samheartache <20imas07@gmail.com>
2025-06-14 14:34:56 +0300

    Fix get_valid_input function and add result prints

--- main.py
+++ main.py
@@ -25,23 +25,28 @@ def main():
         parser.authorize()
         parser.save_results()
 
+        print(f'All data on your albums was saved to {album_save_path} and songs with unknowkn album were saved to {songs_save_path}')
+
     elif choice == '2':
         queries = [album for album in open(get_valid_input('file path where your albums names are saved', validate_textpath), encoding='utf-8')]
         covers_dir = input('Enter directory name where album covers will be saved: ')
         delay = get_valid_input('delay betweem image searching to avoid bot detection', validate_num)
         search_images(queries=queries, covers_dir=covers_dir, delay=int(delay))
+        
         print(f'All album covers were saved in {covers_dir}')
 
     elif choice == '3':
         collage_path = get_valid_input('file name for collage', validate_imagepath)
         covers_dir = input('Enter directory name where your images are saved: ')
-        make_collage(collage_path=collage_path, covers_dir=covers_dir)
+        make_collage(collage_path=collage_path, images_path=covers_dir)
+
+        print(f'Your collage is done and saved as {collage_path}')
 
 
 def get_valid_input(value, validation_func):
     while True:
         user_input = input(f'Enter {value}: ')
-        if validation_func(value):
+        if validation_func(user_input):
             return user_input
         print(f'Please enter {value} correctly ')
 
