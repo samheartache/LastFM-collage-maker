db744f6fffd1c19729f8b1c5ad9c804930a6ca4e
samheartache <20imas07@gmail.com>
2025-09-18 16:18:33 +0300

    Add class LastfmAPI for fast album parsing

--- api.py
+++ api.py
@@ -1,39 +1,99 @@
 import os
+from datetime import datetime, timedelta
 
 import dotenv
 import requests
+from bs4 import BeautifulSoup
 
 dotenv.load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
 
+WEEK = int((datetime.now() - timedelta(weeks=1)).timestamp())
 
-def get_user_tracks(user: str, timestamp: int=0, limit: int=0) -> list:
-    url = f'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={user}&api_key={API_KEY}&format=json&to=1757462400'
-    result_tracks = []
 
-    if timestamp:
-        url += f'&to={timestamp}'
-    if limit:
-        url += f'&limit={limit}'
+class LastfmAPI:
+    def __init__(self, username: str, timestamp=WEEK, limit=1000, album_path='albums.txt', unknown_path='unknown.txt'):
+        self.username = username
+        self.timestamp = timestamp
+        self.limit = limit
+        self.not_found = set()
+        self.album_path = album_path
+        self.unknown_path = unknown_path
+        self.albums = self.get_user_albums()
+
+        self.save_to_files()
     
-    response = requests.get(url=url)
-    response_json = response.json()
-    recent_tracks = response_json['recenttracks']['track']
+    def get_user_albums(self) -> list:
+        url = f'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={self.username}&api_key={API_KEY}&format=json'
+        result_albums = []
+        track_titles = set()
+        album_titles = set()
 
-    for track in recent_tracks:
-        d = {}
-        d['track'] = track['name']
-        d['album'] = track['album']['#text']
-        d['image'] = track['image']
-        d['artist'] = track['artist']['#text']
-        d['url'] = track['url']
+        if self.timestamp:
+            url += f'&from={self.timestamp}'
+        if self.limit:
+            url += f'&limit={self.limit}'
         
-        date = track.get('date', None)
+        response = requests.get(url=url)
+        response_json = response.json()
+        recent_tracks = response_json['recenttracks']['track']
 
-        if date:
-            d['date'] = date['#text']
+        for track in recent_tracks:
+            info = {}
 
-        result_tracks.append(d)
+            info['track'] = track['name']
+            info['artist'] = track['artist']['#text']
+            title = f'{info['artist']} - {info['track']}'
+            if title not in track_titles:
+                track_titles.add(title)
+            else:
+                continue
+
+            album = track['album']['#text']
+            if not album:
+                try:
+                    response = requests.get(url=track['url'], timeout=3)
+                    bs = BeautifulSoup(response.text, 'lxml')
+
+                    album_tag = bs.find('h4', class_='source-album-name')
+                    if album_tag:
+                        album = album_tag.get_text().strip()
+                        info['album'] = album
+                    else:
+                        self.not_found.add(title)
+                        continue
+                except:
+                    continue
+            else:
+                info['album'] = album
+            
+            if album not in album_titles:
+                album_titles.add(album)
+            else:
+                continue
+
+            info['images'] = track['image']
+            images = []
+            for im in info['images']:
+                images.append(im['#text'])
+            info['images'] = images
+
+            date = track.get('date', None)
+            if date:
+                info['date'] = date['#text']
+
+            info['url'] = track['url']
+            
+            result_albums.append(info)
+        
+        return result_albums
     
-    return result_tracks
+    def save_to_files(self):
+        with open(self.album_path, 'w', encoding='utf-8') as file:
+            for track in self.albums:
+                file.write(f'{track['artist']} - {track['album']}, {track['images'][-1]}\n')
+        
+        with open(self.unknown_path, 'w', encoding='utf-8') as file:
+            for track in self.not_found:
+                file.write(track + '\n')
